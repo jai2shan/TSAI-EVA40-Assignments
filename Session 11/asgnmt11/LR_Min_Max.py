a0235f5b38e23ee59c3025f47ac2c9dd0d84f5e4
jai2shan <muralis2raj@gmail.com>
2020-06-14 03:45:44 +0530

    device

--- Session 11/asgnmt11/LR_Min_Max.py
+++ Session 11/asgnmt11/LR_Min_Max.py
@@ -20,6 +20,8 @@ def LR_Max(net,trainloader,start,end,iters,mode):
 
 
 def LR_Min(trainloader,lr_max,model):
+	use_cuda = torch.cuda.is_available()
+	device = torch.device("cuda" if use_cuda else "cpu")
 	mins = [lr_max.values[0]/i for i in list(range(5,11))]
 
 	for i in mins:
