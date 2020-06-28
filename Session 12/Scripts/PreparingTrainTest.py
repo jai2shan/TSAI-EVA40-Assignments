d894cb0a7d04969f0ef4801b187e44d66fff5292
jai2shan <muralis2raj@gmail.com>
2020-06-28 19:27:17 +0530

    remove company name

--- Session 12/Scripts/PreparingTrainTest.py
+++ Session 12/Scripts/PreparingTrainTest.py
@@ -2,18 +2,18 @@
 """
 Created on Sat Jun 27 15:48:51 2020
 
-@author: jayasans4085
+@author: ***
 """
 
 import os
-os.chdir(r'C:\Users\jayasans4085\OneDrive - ARCADIS\Desktop\TSAI Sessions\tiny-imagenet-200\tiny-imagenet-200\train')
-basepath = r'C:\Users\jayasans4085\OneDrive - ARCADIS\Desktop\TSAI Sessions\tiny-imagenet-200 - Copy\tiny-imagenet-200\train'
+os.chdir(r'C:\Users\jayasans4085\OneDrive - ***\Desktop\TSAI Sessions\tiny-imagenet-200\tiny-imagenet-200\train')
+basepath = r'C:\Users\jayasans4085\OneDrive - ***\Desktop\TSAI Sessions\tiny-imagenet-200 - Copy\tiny-imagenet-200\train'
 import random
 import shutil
 #train=data.sample(frac=0.8,random_state=200)
 j = 0
-train = r'C:\Users\jayasans4085\OneDrive - ARCADIS\Desktop\TSAI Sessions\tiny-imagenet-200 - Copy\tiny-imagenet-200\AssignmentData\train'
-test = r'C:\Users\jayasans4085\OneDrive - ARCADIS\Desktop\TSAI Sessions\tiny-imagenet-200 - Copy\tiny-imagenet-200\AssignmentData\test'
+train = r'C:\Users\jayasans4085\OneDrive - ***\Desktop\TSAI Sessions\tiny-imagenet-200 - Copy\tiny-imagenet-200\AssignmentData\train'
+test = r'C:\Users\jayasans4085\OneDrive - ***\Desktop\TSAI Sessions\tiny-imagenet-200 - Copy\tiny-imagenet-200\AssignmentData\test'
 
 classes = dict()
 j = 0
